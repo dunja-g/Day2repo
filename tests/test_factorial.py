9fe8820c8449911a9d628e30bfcffa11fb373cd5
Dunja Gvozdenovic <dg2u25@soton.ac.uk>
2026-03-24 10:17:10 +0000

    Add precondition

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -4,3 +4,13 @@ from mymath.factorial import factorial
 
 def test_3():
     assert factorial(3) == 6
+
+def test_2():
+    assert factorial(2) == 2
+
+def test_0():
+    assert factorial(0) == 1
+
+def test_negative():
+    with pytest.raises(ValueError):
+        factorial(-1)
